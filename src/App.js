716c437e6486c4e9586f50c3ef9a9c919d113f0c
Claudia <caucrah@gmail.com>
2021-11-12 00:32:50 +0100

    Finished the application

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+import AddTransactions from './components/AddTransactions';
+
+import { GlobalProvider } from './context/GlobalState';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransactions />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
